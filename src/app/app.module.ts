22f7270bb21594f4bc6fc2163b3d69dada836c9d
Sushant <sushant.srivastav@gmail.com>
2018-08-23 20:58:35 +0530

    Added Search Component

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,16 +4,16 @@ import {NgModule} from '@angular/core';
 
 import {AppComponent} from './app.component';
 import {AppService} from './app.service';
-import {TestComponent} from './test/test.component';
+import {SearchComponent} from './test/search.component';
 
 
 @NgModule({
   declarations: [
     AppComponent,
-    TestComponent
+    SearchComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
   ],
   providers: [AppService],
   bootstrap: [AppComponent]
